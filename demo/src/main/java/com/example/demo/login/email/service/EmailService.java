420d361854bdd7c5d68c37ab2a62a2abafc40883
승엽 <tmdduqflfl@naver.com>
2025-04-29 17:04:42 +0900

    Refactoring : Service - Domain 로직 분리 책임 분명

--- demo/src/main/java/com/example/demo/login/email/service/EmailService.java
+++ demo/src/main/java/com/example/demo/login/email/service/EmailService.java
@@ -1,5 +1,9 @@
 package com.example.demo.login.email.service;
 
+import com.example.demo.login.email.domain.EmailCode;
+
+import com.example.demo.login.email.infrastruture.EmailForm;
+
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 
@@ -8,54 +12,24 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.context.Context;
-import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.io.UnsupportedEncodingException;
-import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
     private final JavaMailSender emailSender;
-    private final SpringTemplateEngine templateEngine;
+
+    private final EmailCode emailCode;
+    private final EmailForm form;
 
     public String sendEmail(String toEmail, HttpSession session) throws MessagingException, UnsupportedEncodingException {
-        String authNum = createCode();
+        String authNum = emailCode.createCode();
         session.setAttribute(toEmail, authNum);
-
-        MimeMessage emailForm = createEmailForm(toEmail, authNum);
+        MimeMessage emailForm = form.createEmailForm(toEmail, authNum);
         emailSender.send(emailForm);
         return authNum;
     }
 
-    private String createCode() {
-        Random random = new Random();
-        StringBuilder key = new StringBuilder();
-        for (int i = 0; i < 8; i++) {
-            int index = random.nextInt(3);
-            switch (index) {
-                case 0: key.append((char) (random.nextInt(26) + 97)); break;
-                case 1: key.append((char) (random.nextInt(26) + 65)); break;
-                case 2: key.append(random.nextInt(10)); break;
-            }
-        }
-        return key.toString();
-    }
-
-    private MimeMessage createEmailForm(String email, String authNum) throws MessagingException, UnsupportedEncodingException {
-        MimeMessage message = emailSender.createMimeMessage();
-        message.addRecipients(MimeMessage.RecipientType.TO, email);
-        message.setSubject("인증번호");
-        message.setFrom("tmdduqflfl@naver.com");
-        message.setText(setContext(authNum), "utf-8", "html");
-        return message;
-    }
-
-    private String setContext(String code) {
-        Context context = new Context();
-        context.setVariable("code", code);
-        return templateEngine.process("mail", context);
-    }
 }
