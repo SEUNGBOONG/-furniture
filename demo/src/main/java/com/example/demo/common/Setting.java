57502a4f71c991287bc88abb6ab03b63fa4d102b
승엽 <tmdduqflfl@naver.com>
2025-04-30 01:58:13 +0900

    Refactoring: ENUM 추가

--- demo/src/main/java/com/example/demo/common/Setting.java
+++ demo/src/main/java/com/example/demo/common/Setting.java
@@ -6,7 +6,28 @@ public enum Setting {
     SUCCEED_SENDER_CERTIFICATION_NUMBER("인증 코드가 전송되었습니다."),
     SUCCEED_CERTIFICATION_NUMBER("인증 성공!"),
     FAIL_CERTIFICATION_NUMBER("인증번호가 틀립니다."),
-    PLEASE_COMPLETE_EMAIL_VERIFICATION_FIRST("이메일 인증을 먼저 완료해주세요.");
+    PLEASE_COMPLETE_EMAIL_VERIFICATION_FIRST("이메일 인증을 먼저 완료해주세요."),
+
+    // ✅ Category 관련 메시지
+    CATEGORY_CREATE_SUCCESS("카테고리 등록 완료"),
+    CATEGORY_UPDATE_SUCCESS("카테고리 수정 완료"),
+    CATEGORY_DELETE_SUCCESS("카테고리 삭제 완료"),
+    FORBIDDEN_ONLY_ADMIN("관리자만 등록 가능합니다."),
+
+
+    // 상품 관련 메시지
+    PRODUCT_CREATE_SUCCESS("상품 등록 완료"),
+    PRODUCT_UPDATE_SUCCESS("상품 수정 완료"),
+    PRODUCT_DELETE_SUCCESS("상품 삭제 완료"),
+
+
+    // Category 도메인 메시지
+    CATEGORY_NAME_REQUIRED("카테고리 이름은 필수입니다."),
+    CATEGORY_NAME_NOT_EMPTY("카테고리 이름은 비어 있을 수 없습니다."),
+    PRODUCT_NOT_FOUND("존재하지 않는 상품입니다."),
+    NOT_FOUND_CATEGORY( "카테고리를 찾을 수 없습니다."),
+    CATEGORY_NOT_FOUND("존재하지 않는 카테고리입니다.");
+
 
     private final String setting;
 
@@ -18,5 +39,4 @@ public enum Setting {
     public String toString() {
         return setting;
     }
-
 }
