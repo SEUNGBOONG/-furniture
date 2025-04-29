05cbcad39a2a7237282b008efb213f6061c4700c
승엽 <tmdduqflfl@naver.com>
2025-04-30 01:57:49 +0900

    Feat : 상품 DTO

---
+++ demo/src/main/java/com/example/demo/product/controller/dto/ProductResponse.java
@@ -0,0 +1,14 @@
+package com.example.demo.product.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ProductResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private int price;
+    private String categoryName;
+}
