2723033231971640b20d85ac1f429548770a55d7
승엽 <tmdduqflfl@naver.com>
2025-04-30 01:57:05 +0900

    Feat : 카테고리 DTO

---
+++ demo/src/main/java/com/example/demo/product/controller/dto/CategoryResponse.java
@@ -0,0 +1,16 @@
+package com.example.demo.product.controller.dto;
+
+import com.example.demo.product.domain.entity.Category;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CategoryResponse {
+    private Long id;
+    private String name;
+
+    public static CategoryResponse from(Category category) {
+        return new CategoryResponse(category.getId(), category.getName());
+    }
+}
