ee9c26a6ba037aed18c8b5ff08ad88da3de1a079
승엽 <tmdduqflfl@naver.com>
2025-04-30 01:56:39 +0900

    Feat : 카테고리 엔티티 생성 카테고리 -> 상품 조회

---
+++ demo/src/main/java/com/example/demo/product/domain/entity/Category.java
@@ -0,0 +1,44 @@
+package com.example.demo.product.domain.entity;
+
+import com.example.demo.common.Setting;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "카테고리 이름은 필수입니다.")
+    private String name;
+
+    public Category(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException(Setting.CATEGORY_NAME_NOT_EMPTY.toString());
+        }
+        this.name = name;
+    }
+
+    public void updateName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException(Setting.CATEGORY_NAME_NOT_EMPTY.toString());
+        }
+        this.name = name;
+    }
+
+}
