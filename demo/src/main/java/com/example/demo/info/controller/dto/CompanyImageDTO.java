cf95c04a4c98154876b0a29945562a042531d87b
승엽 <tmdduqflfl@naver.com>
2025-05-03 13:02:03 +0900

    Feat: 회사소개 인사말 DTO

---
+++ demo/src/main/java/com/example/demo/info/controller/dto/CompanyImageDTO.java
@@ -0,0 +1,11 @@
+package com.example.demo.info.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CompanyImageDTO {
+    private String imageUrl;
+    private String description;
+}
