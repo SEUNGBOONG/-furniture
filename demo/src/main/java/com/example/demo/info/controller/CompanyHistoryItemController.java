c13466f9235edc95bed336478714e2a78d597523
승엽 <tmdduqflfl@naver.com>
2025-05-03 14:46:07 +0900

    Feat: 오시는 길 컨트롤러

---
+++ demo/src/main/java/com/example/demo/info/controller/CompanyHistoryItemController.java
@@ -0,0 +1,26 @@
+package com.example.demo.info.controller;
+
+import com.example.demo.info.controller.dto.CompanyHistoryItemDTO;
+import com.example.demo.info.service.CompanyHistoryItemService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/company")
+public class CompanyHistoryItemController {
+
+    private final CompanyHistoryItemService service;
+
+    public CompanyHistoryItemController(CompanyHistoryItemService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/history")
+    public ResponseEntity<List<CompanyHistoryItemDTO>> getHistoryItems() {
+        return ResponseEntity.ok(service.getAllHistoryItems());
+    }
+}
