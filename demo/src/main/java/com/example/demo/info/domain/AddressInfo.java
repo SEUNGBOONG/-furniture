2a2219a7a216b327f61d573d050f78dd38166454
승엽 <tmdduqflfl@naver.com>
2025-05-03 14:35:34 +0900

    Feat: 오시는 길 데이터 삽입 및 반환

---
+++ demo/src/main/java/com/example/demo/info/domain/AddressInfo.java
@@ -0,0 +1,33 @@
+package com.example.demo.info.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "address_info") // 실제 MySQL 테이블명
+public class AddressInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String address;
+    private String phone;
+    private String fax;
+    private String email;
+    private String subway;
+    private String bus;
+    private String walk;
+    private String car;
+}
