0e56d538dc81c39ee836862044665b2abd01db10
승엽 <tmdduqflfl@naver.com>
2025-05-10 15:20:26 +0900

    Feat : Service 기능

--- demo/src/main/java/com/example/demo/product/service/ProductService.java
+++ demo/src/main/java/com/example/demo/product/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.example.demo.product.service;
 
 import com.example.demo.common.Setting;
+import com.example.demo.config.s3.S3Uploader;
 import com.example.demo.product.controller.dto.CategoryResponse;
 import com.example.demo.product.controller.dto.ProductRequest;
 import com.example.demo.product.controller.dto.ProductResponse;
@@ -10,6 +11,7 @@ import com.example.demo.product.domain.entity.Product;
 import com.example.demo.product.domain.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,25 +22,31 @@ public class ProductService {
 
     private final ProductRepository productRepository;
     private final CategoryRepository categoryRepository;
+    private final S3Uploader s3Uploader;
 
-    public void createProduct(ProductRequest request) {
-        Category category = categoryRepository.findById(request.getCategoryId())
-                .orElseThrow(() -> new IllegalArgumentException(Setting.CATEGORY_NOT_FOUND.toString()));
+    public void createProduct(ProductRequest request, MultipartFile imageFile) {
+        Category category = getCategory(request);
+        String imageUrl = s3Uploader.uploadFile(imageFile);
 
-        Product product = Product.builder()
+        Product product = getProduct(Product.builder(), request, category, imageUrl);
+
+        productRepository.save(product);
+    }
+
+    private static Product getProduct(final Product.ProductBuilder builder, final ProductRequest request, final Category category, final String imageUrl) {
+        return builder
                 .name(request.getName())
                 .description(request.getDescription())
                 .price(request.getPrice())
                 .category(category)
                 .tagName(request.getTag())
+                .image(imageUrl)
                 .build();
-
-        productRepository.save(product);
     }
 
     public List<ProductResponse> getProductsByTagName(String tagName) {
         return productRepository.findByTagName(tagName).stream()
-                .map(p -> new ProductResponse(p.getId(), p.getName(), p.getDescription(), p.getPrice(), p.getCategory().getName()))
+                .map(p -> new ProductResponse(p.getId(), p.getName(), p.getDescription(), p.getPrice(), p.getCategory().getName(),p.getImage()))
                 .collect(Collectors.toList());
     }
 
@@ -50,39 +58,45 @@ public class ProductService {
 
     public List<ProductResponse> getProductsByCategory(Long categoryId) {
         return productRepository.findByCategoryId(categoryId).stream()
-                .map(p -> new ProductResponse(p.getId(), p.getName(), p.getDescription(), p.getPrice(), p.getCategory().getName()))
+                .map(p -> new ProductResponse(p.getId(), p.getName(), p.getDescription(), p.getPrice(), p.getCategory().getName(),p.getImage()))
                 .collect(Collectors.toList());
     }
 
     public ProductResponse getProductDetail(Long productId) {
-        Product product = productRepository.findById(productId)
-                .orElseThrow(() -> new IllegalArgumentException(Setting.PRODUCT_NOT_FOUND.toString()));
-        return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice(), product.getCategory().getName());
+        Product product = getProduct(productId);
+        return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice(), product.getCategory().getName(), product.getImage());
     }
 
-    public void updateProduct(Long productId, ProductRequest request) {
-        Product product = productRepository.findById(productId)
-                .orElseThrow(() -> new IllegalArgumentException(Setting.PRODUCT_NOT_FOUND.toString()));
+    public void updateProduct(Long productId, ProductRequest request, MultipartFile imageFile) {
+        Product product = getProduct(productId);
+        Category category = getCategory(request);
+        Result result = new Result(product, category);
 
-        Category category = categoryRepository.findById(request.getCategoryId())
+        String imageUrl = s3Uploader.uploadFile(imageFile); // 이미지 다시 업로드
+
+        Product updated = getProduct(Product.builder()
+                .id(result.product().getId()), request, result.category(), imageUrl);
+
+        productRepository.save(updated);
+    }
+
+    private Category getCategory(final ProductRequest request) {
+        return categoryRepository.findById(request.getCategoryId())
                 .orElseThrow(() -> new IllegalArgumentException(Setting.CATEGORY_NOT_FOUND.toString()));
-
-        product = Product.builder()
-                .id(product.getId())
-                .name(request.getName())
-                .description(request.getDescription())
-                .price(request.getPrice())
-                .category(category)
-                .tagName(request.getTag())
-                .build();
-
-        productRepository.save(product);
     }
+
+    private Product getProduct(final Long productId) {
+        return productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException(Setting.PRODUCT_NOT_FOUND.toString()));
+    }
+
 
     public void deleteProduct(Long productId) {
-        Product product = productRepository.findById(productId)
-                .orElseThrow(() -> new IllegalArgumentException(Setting.PRODUCT_NOT_FOUND.toString()));
+        Product product = getProduct(productId);
         productRepository.delete(product);
+    }
+
+    private record Result(Product product, Category category) {
     }
 
 }
