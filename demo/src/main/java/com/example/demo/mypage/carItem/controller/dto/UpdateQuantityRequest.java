50b99a651afe0bba07cd3f72a2cd1b527b975991
승엽 <tmdduqflfl@naver.com>
2025-05-19 20:21:04 +0900

    Feat: Request

---
+++ demo/src/main/java/com/example/demo/mypage/carItem/controller/dto/UpdateQuantityRequest.java
@@ -0,0 +1,11 @@
+package com.example.demo.mypage.carItem.controller.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UpdateQuantityRequest {
+    private Long cartItemId;  // CartItem.id 로 조회
+    private int quantity;
+}
