0ceb31029af5b079ed0a814dfa9e1454cb4035bd
승엽 <tmdduqflfl@naver.com>
2025-05-19 19:16:33 +0900

    Feat: 객체지향 Clean code

--- demo/src/main/java/com/example/demo/mypage/carItem/service/CartService.java
+++ demo/src/main/java/com/example/demo/mypage/carItem/service/CartService.java
@@ -1,11 +1,15 @@
 package com.example.demo.mypage.carItem.service;
 
 import com.example.demo.login.member.domain.member.Member;
+
 import com.example.demo.login.member.infrastructure.member.MemberJpaRepository;
+
 import com.example.demo.mypage.carItem.domain.entity.CartItem;
 import com.example.demo.mypage.carItem.domain.repository.CartItemRepository;
+
 import com.example.demo.product.domain.entity.Product;
 import com.example.demo.product.domain.repository.ProductRepository;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,22 +19,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CartService {
 
+    public static final String NOT_FOUND_MEMBER = "회원 없음";
+    public static final String NOT_FOUND_PRODUCT = "상품 없음";
+
     private final CartItemRepository cartItemRepository;
     private final ProductRepository productRepository;
     private final MemberJpaRepository memberRepository;
 
     public void addToCart(Long productId, int quantity, Long memberId) {
-        Product product = productRepository.findById(productId)
-                .orElseThrow(() -> new RuntimeException("상품 없음"));
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new RuntimeException("회원 없음"));
+        Product product = findPorduct(productId);
+        Member member = findMemberId(memberId);
 
-        CartItem cartItem = cartItemRepository.findByMemberAndProduct(member, product)
-                .orElse(CartItem.builder()
-                        .member(member)
-                        .product(product)
-                        .quantity(0)
-                        .build());
+        CartItem cartItem = findCartItemProductId(member, product);
 
         cartItem.increaseQuantity(quantity);
         cartItemRepository.save(cartItem);
@@ -45,10 +45,28 @@ public class CartService {
     }
 
     public void deleteSelectedItems(Long memberId, List<Long> productIds) {
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new RuntimeException("회원 없음"));
+        Member member = findMemberId(memberId);
 
         List<CartItem> items = cartItemRepository.findByMemberAndProductIdIn(member, productIds);
         cartItemRepository.deleteAll(items);
+    }
+
+    private Member findMemberId(final Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new RuntimeException(NOT_FOUND_MEMBER));
+    }
+
+    private Product findPorduct(final Long productId) {
+        return productRepository.findById(productId)
+                .orElseThrow(() -> new RuntimeException(NOT_FOUND_PRODUCT));
+    }
+
+    private CartItem findCartItemProductId(final Member member, final Product product) {
+        return cartItemRepository.findByMemberAndProduct(member, product)
+                .orElse(CartItem.builder()
+                        .member(member)
+                        .product(product)
+                        .quantity(0)
+                        .build());
     }
 }
