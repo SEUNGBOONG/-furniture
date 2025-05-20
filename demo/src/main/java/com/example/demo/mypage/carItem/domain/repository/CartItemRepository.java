a12eab233ade011d30e63a681cecf66709af0dbc
승엽 <tmdduqflfl@naver.com>
2025-05-20 20:23:58 +0900

    Feat: 직접 쿼리를 통한 성능개선

--- demo/src/main/java/com/example/demo/mypage/carItem/domain/repository/CartItemRepository.java
+++ demo/src/main/java/com/example/demo/mypage/carItem/domain/repository/CartItemRepository.java
@@ -4,19 +4,36 @@ import com.example.demo.login.member.domain.member.Member;
 import com.example.demo.mypage.carItem.domain.entity.CartItem;
 import com.example.demo.product.domain.entity.Product;
 import io.lettuce.core.dynamic.annotation.Param;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-    Optional<CartItem> findByMemberAndProduct(Member member, Product product);
-    List<CartItem> findAllByMemberId(Long memberId);
     void deleteByMemberId(Long memberId);
     List<CartItem> findByMemberAndProductIdIn(Member member, List<Long> productIds);
 
-    @Query("SELECT c FROM CartItem c JOIN FETCH c.product JOIN FETCH c.member WHERE c.member = :member AND c.product = :product")
-    Optional<CartItem> findByMemberAndProductWithFetch(@Param("member") Member member, @Param("product") Product product);
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO cart_item (product_id, member_id, quantity) " +
+            "VALUES (:productId, :memberId, :quantity) " +
+            "ON DUPLICATE KEY UPDATE quantity = quantity + :quantity", nativeQuery = true)
+    void upsertCartItem(Long productId, Long memberId, int quantity);
+
+    @Query("SELECT DISTINCT c FROM CartItem c JOIN FETCH c.product WHERE c.member.id = :memberId")
+    List<CartItem> findDistinctByMemberId(Long memberId);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE cart_item SET quantity = quantity + :amount WHERE id = :cartItemId AND member_id = :memberId", nativeQuery = true)
+    int updateQuantityByAmount(@Param("cartItemId") Long cartItemId,
+                               @Param("memberId") Long memberId,
+                               @Param("amount") int amount);
+
+
+    @Query(value = "SELECT EXISTS(SELECT 1 FROM cart_item WHERE member_id = :memberId AND product_id = :productId)", nativeQuery = true)
+    Long existsCartItem(@Param("memberId") Long memberId, @Param("productId") Long productId);
 
 }
