e42b9039e3c7381aab2045cd5842896e84c8f92f
승엽 <tmdduqflfl@naver.com>
2025-05-20 20:22:40 +0900

    Feat: 장바구니 Controller

--- demo/src/main/java/com/example/demo/mypage/carItem/controller/CartController.java
+++ demo/src/main/java/com/example/demo/mypage/carItem/controller/CartController.java
@@ -1,19 +1,20 @@
 package com.example.demo.mypage.carItem.controller;
 
 import com.example.demo.login.global.annotation.Member;
+
 import com.example.demo.mypage.carItem.controller.dto.AddCartRequest;
 import com.example.demo.mypage.carItem.controller.dto.CartItemDeleteRequest;
+import com.example.demo.mypage.carItem.controller.dto.CartItemIdRequest;
 import com.example.demo.mypage.carItem.controller.dto.CartItemResponse;
 import com.example.demo.mypage.carItem.controller.dto.CartSummaryResponse;
 import com.example.demo.mypage.carItem.controller.dto.UpdateQuantityRequest;
+
 import com.example.demo.mypage.carItem.domain.Amount;
 import com.example.demo.mypage.carItem.domain.entity.CartItem;
 import com.example.demo.mypage.carItem.service.CartService;
 
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +26,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/cart")
@@ -36,7 +36,6 @@ public class CartController {
     @PostMapping("/add")
     public ResponseEntity<Void> addToCart(@RequestBody AddCartRequest request,
                                           @Member Long memberId) {
-
         cartService.addToCart(request.getProductId(), request.getQuantity(), memberId);
         return ResponseEntity.ok().build();
     }
@@ -48,16 +47,28 @@ public class CartController {
         return ResponseEntity.ok().build();
     }
 
-
     @GetMapping
     public ResponseEntity<CartSummaryResponse> getCartItems(@Member Long memberId) {
         List<CartItem> cartItems = cartService.getCartItems(memberId);
         List<CartItemResponse> items = getCartItemResponses(cartItems);
-
         Amount totalAmount = Amount.calculateTotalAmount(items);
-
         return ResponseEntity.ok(new CartSummaryResponse(items, totalAmount.getTotalAmount()));
     }
+
+    @PostMapping("/increase")
+    public ResponseEntity<Void> increaseQuantity(@RequestBody CartItemIdRequest request,
+                                                 @Member Long memberId) {
+        cartService.increaseQuantity(memberId, request.getCartItemId());
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/decrease")
+    public ResponseEntity<Void> decreaseQuantity(@RequestBody CartItemIdRequest request,
+                                                 @Member Long memberId) {
+        cartService.decreaseQuantity(memberId, request.getCartItemId());
+        return ResponseEntity.ok().build();
+    }
+
 
     @DeleteMapping("/clear")
     public ResponseEntity<Void> clearCart(@Member Long memberId) {
@@ -75,6 +86,7 @@ public class CartController {
     private static List<CartItemResponse> getCartItemResponses(final List<CartItem> cartItems) {
         return cartItems.stream()
                 .map(item -> new CartItemResponse(
+                        item.getId(),
                         item.getProduct().getId(),
                         item.getProduct().getName(),
                         item.getProduct().getPrice(),
