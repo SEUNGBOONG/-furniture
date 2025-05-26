28ec4dca89cc490a525f4221e821d8ccbbc1c1ac
승엽 <tmdduqflfl@naver.com>
2025-05-26 20:09:49 +0900

    Refactoring: entity repository 구별 책임

--- demo/src/main/java/com/example/demo/info/domain/repository/AddressInfoRepository.java
+++ demo/src/main/java/com/example/demo/info/domain/repository/AddressInfoRepository.java
@@ -1,5 +1,6 @@
-package com.example.demo.info.domain;
+package com.example.demo.info.domain.repository;
 
+import com.example.demo.info.domain.entity.AddressInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AddressInfoRepository extends JpaRepository<AddressInfo, Long> {
