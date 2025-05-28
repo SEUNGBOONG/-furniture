769dacb32768c052b4c83c5dfd85d4466c9518c6
승엽 <tmdduqflfl@naver.com>
2025-05-28 17:15:07 +0900

    Feat: post  get

---
+++ demo/src/main/java/com/example/demo/info/controller/dto/ImageUploadRequest.java
@@ -0,0 +1,8 @@
+package com.example.demo.info.controller.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class ImageUploadRequest {
+    private MultipartFile file;  // 파일만 받음
+    // getter/setter
+}
