978a3d619e1c9c86bf5f98ac5c3354ab81ad26d6
승엽 <tmdduqflfl@naver.com>
2025-05-30 02:09:23 +0900

    Feat : 분리 DTO

---
+++ demo/src/main/java/com/example/demo/login/member/controller/auth/dto/NormalSignUpRequest.java
@@ -0,0 +1,12 @@
+package com.example.demo.login.member.controller.auth.dto;
+
+public record NormalSignUpRequest(
+        String memberEmail,
+        String memberName,
+        String memberPassword,
+        String memberNickName,
+        String roadAddress,
+        String jibunAddress,
+        String zipCode
+) {
+}
