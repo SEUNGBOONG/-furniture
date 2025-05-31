9beee366df515528b0e0fc5eee1010c83c147fd8
승엽 <tmdduqflfl@naver.com>
2025-05-31 17:20:51 +0900

    Feat: 비밀번호 인코딩

--- demo/src/main/java/com/example/demo/login/member/mapper/auth/AuthMapper.java
+++ demo/src/main/java/com/example/demo/login/member/mapper/auth/AuthMapper.java
@@ -1,18 +1,17 @@
 package com.example.demo.login.member.mapper.auth;
 
 import com.example.demo.login.member.controller.auth.dto.LoginResponse;
+import com.example.demo.login.member.controller.auth.dto.NormalSignUpRequest;
 import com.example.demo.login.member.controller.auth.dto.SignUpRequest;
 import com.example.demo.login.member.controller.auth.dto.SignUpResponse;
 import com.example.demo.login.member.domain.member.Member;
-
 public class AuthMapper {
 
-    // AuthMapper.java
-    public static Member toMember(SignUpRequest signUpRequest, String imageUrl) {
+    public static Member toMember(SignUpRequest signUpRequest, String encodedPassword, String imageUrl) {
         return Member.builder()
                 .memberEmail(signUpRequest.memberEmail())
                 .memberName(signUpRequest.memberName())
-                .memberPassword(signUpRequest.memberPassword())
+                .memberPassword(encodedPassword)
                 .memberNickName(signUpRequest.memberNickName())
                 .roadAddress(signUpRequest.roadAddress())
                 .jibunAddress(signUpRequest.jibunAddress())
@@ -23,11 +22,11 @@ public class AuthMapper {
                 .build();
     }
 
-    public static Member toNormalMember(SignUpRequest request) {
+    public static Member toNormalMember(NormalSignUpRequest request, String encodedPassword) {
         return Member.builder()
                 .memberEmail(request.memberEmail())
                 .memberName(request.memberName())
-                .memberPassword(request.memberPassword())
+                .memberPassword(encodedPassword)  // 수정
                 .memberNickName(request.memberNickName())
                 .roadAddress(request.roadAddress())
                 .jibunAddress(request.jibunAddress())
@@ -37,8 +36,12 @@ public class AuthMapper {
     }
 
     public static SignUpResponse toSignUpResponse(Member member) {
-        return new SignUpResponse(member.getId(), member.getMemberName(), member.getMemberEmail(),
-                member.getMemberPassword(), member.getMemberNickName());
+        return new SignUpResponse(
+                member.getId(),
+                member.getMemberName(),
+                member.getMemberEmail(),
+                member.getMemberNickName()
+        );
     }
 
     public static LoginResponse toLoginResponse(String token, Member member) {
