8dae7340c464b8c0cb6657dfc0e9f050cb1977f8
승엽 <tmdduqflfl@naver.com>
2025-06-04 18:24:39 +0900

    Refactoring :

--- demo/src/main/java/com/example/demo/info/controller/AddressInfoController.java
+++ demo/src/main/java/com/example/demo/info/controller/AddressInfoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/address")
+@RequestMapping("/address")
 public class AddressInfoController {
 
     private final AddressInfoRepository repository;
