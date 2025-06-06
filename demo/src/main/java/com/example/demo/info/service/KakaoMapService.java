7b5356201da17fd3f686bc5ccdec7591bab71c94
승엽 <tmdduqflfl@naver.com>
2025-06-06 18:44:16 +0900

    Refactoring : 상수화

--- demo/src/main/java/com/example/demo/info/service/KakaoMapService.java
+++ demo/src/main/java/com/example/demo/info/service/KakaoMapService.java
@@ -11,6 +11,13 @@ import org.json.JSONObject;
 @Service
 public class KakaoMapService {
 
+    public static final String URL = "https://dapi.kakao.com/v2/local/search/address.json?query=";
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String KAKAO_AK = "KakaoAK ";
+    public static final String DOCUMENTS = "documents";
+    public static final String Y = "y";
+    public static final String X = "x";
+
     private final RestTemplate restTemplate;
 
     @Value("${kakao.client.id}")
@@ -21,22 +28,25 @@ public class KakaoMapService {
     }
 
     public String getCoordinatesFromAddress(String address) {
-        String url = "https://dapi.kakao.com/v2/local/search/address.json?query=" + address;
+        String url = URL + address;
 
         // API 호출을 위한 헤더 설정
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "KakaoAK " + apiKey);  // 'KakaoAK' 뒤에 API 키 추가
+        headers.set(AUTHORIZATION, KAKAO_AK + apiKey);  // 'KakaoAK' 뒤에 API 키 추가
 
         // HTTP 요청 보내기
         HttpEntity<String> entity = new HttpEntity<>(headers);
-        ResponseEntity<String> response = restTemplate.exchange(url, org.springframework.http.HttpMethod.GET, entity, String.class);
+        ResponseEntity<String> response = restTemplate.exchange(url,
+                org.springframework.http.HttpMethod.GET,
+                entity,
+                String.class);
 
         // 응답에서 위도와 경도 추출 (JSON 파싱)
         JSONObject jsonResponse = new JSONObject(response.getBody());
-        JSONObject firstResult = jsonResponse.getJSONArray("documents").getJSONObject(0);
+        JSONObject firstResult = jsonResponse.getJSONArray(DOCUMENTS).getJSONObject(0);
 
-        double lat = firstResult.getDouble("y");  // 위도
-        double lon = firstResult.getDouble("x");  // 경도
+        double lat = firstResult.getDouble(Y);  // 위도
+        double lon = firstResult.getDouble(X);  // 경도
 
         return lat + "," + lon;  // "위도,경도" 형식으로 반환
     }
