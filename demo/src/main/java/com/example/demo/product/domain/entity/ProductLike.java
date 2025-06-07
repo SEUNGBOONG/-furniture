6190faa7941b9fbb36c01d564765a443bc638304
승엽 <tmdduqflfl@naver.com>
2025-06-07 19:53:18 +0900

    Refactoring : service -> util

---
+++ demo/src/main/java/com/example/demo/product/domain/entity/ProductLike.java
@@ -0,0 +1,44 @@
+package com.example.demo.product.domain.entity;
+
+import com.example.demo.login.member.domain.member.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class ProductLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 회원
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    // 상품
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public static ProductLike of(Member member, Product product) {
+        return ProductLike.builder()
+                .member(member)
+                .product(product)
+                .build();
+    }
+}
