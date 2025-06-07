deb8a62a63231d4041df8c2a644b3ce3c2f09911
승엽 <tmdduqflfl@naver.com>
2025-06-07 15:52:57 +0900

    Feat:  Domain 분리

--- demo/src/main/java/com/example/demo/info/controller/MapController.java
+++ demo/src/main/java/com/example/demo/info/controller/MapController.java
@@ -1,9 +1,9 @@
 package com.example.demo.info.controller;
 
+import com.example.demo.common.Setting;
 import com.example.demo.info.domain.MapValidator;
 import com.example.demo.info.service.KakaoMapService;
 
-import lombok.Getter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,12 +21,12 @@ public class MapController {
 
     @GetMapping("/map")
     public ResponseEntity<Object> getMapCoordinates() {
-        String address = "경기도 화성시 장안면 돌서지길 132-46";  // 예시 주소
+        String address = Setting.ADDRESS.toString();  // 예시 주소
         String coordinates = kakaoMapService.getCoordinatesFromAddress(address);
         // "위도,경도" 문자열을 쉼표로 분리하여 JSON 객체로 반환
         String[] coords = mapValidator.splitCoordinates(coordinates);
-        double lat = Double.parseDouble(coords[0]);
-        double lon = Double.parseDouble(coords[1]);
+        double lat = mapValidator.parseDouble(coords[0]);
+        double lon = mapValidator.parseDouble(coords[1]);
 
         // JSON 형식으로 반환
         return ResponseEntity.ok().body(new Coordinates(lat, lon));
