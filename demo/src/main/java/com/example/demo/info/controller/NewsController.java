6afefd8198787a77b0632a1dff2cede594a15825
승엽 <tmdduqflfl@naver.com>
2025-06-07 19:12:46 +0900

    Refactoring : 명확한 책임 부여

--- demo/src/main/java/com/example/demo/info/controller/NewsController.java
+++ demo/src/main/java/com/example/demo/info/controller/NewsController.java
@@ -1,6 +1,6 @@
 package com.example.demo.info.controller;
 
-import com.example.demo.info.service.NewsService;
+import com.example.demo.info.util.InformationProvider;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,14 +9,14 @@ import java.util.List;
 @RestController
 public class NewsController {
 
-    private final NewsService newsService;
+    private final InformationProvider informationProvider;
 
-    public NewsController(final NewsService newsService) {
-        this.newsService = newsService;
+    public NewsController(final InformationProvider informationProvider) {
+        this.informationProvider = informationProvider;
     }
 
     @GetMapping("/news")
     public List<String> getNewsUrls() {
-        return newsService.fetchAllNewsUrls();
+        return informationProvider.fetchAllNewsUrls();
     }
 }
