d0d865f5b5c5adf9fcf5b221db94d02c0b51a2c5
승엽 <tmdduqflfl@naver.com>
2025-06-07 19:06:07 +0900

    Refactoring : 책임 분할

--- demo/src/main/java/com/example/demo/info/controller/PromoteVideoController.java
+++ demo/src/main/java/com/example/demo/info/controller/PromoteVideoController.java
@@ -1,23 +1,25 @@
 package com.example.demo.info.controller;
 
+import com.example.demo.info.util.VideUtilToYoutube;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping
 public class PromoteVideoController {
 
+    private final VideUtilToYoutube videoInfo;
+
+    public PromoteVideoController(final VideUtilToYoutube videoInfo) {
+        this.videoInfo = videoInfo;
+    }
+
     @GetMapping("/video-info")
     public ResponseEntity<Map<String, String>> getVideoInfo() {
-        Map<String, String> videoInfo = new HashMap<>();
-        videoInfo.put("title", "대명 공장 제품영상");
-        videoInfo.put("Date", "2020-06-27");
-        videoInfo.put("videoUrl", "https://www.youtube.com/watch?v=Q4WZCZoQ6cE");
-        return ResponseEntity.ok(videoInfo);
+        return ResponseEntity.ok(videoInfo.getVideoInfo());
     }
 }
