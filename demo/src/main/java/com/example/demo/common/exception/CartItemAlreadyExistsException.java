08ca66373455c53a751bef84815f7662b683d280
승엽 <tmdduqflfl@naver.com>
2025-06-18 18:47:29 +0900

    Refactoring 안쓰는 로직 삭제

--- demo/src/main/java/com/example/demo/common/exception/CartItemAlreadyExistsException.java
+++ demo/src/main/java/com/example/demo/common/exception/CartItemAlreadyExistsException.java
@@ -4,8 +4,4 @@ public class CartItemAlreadyExistsException extends RuntimeException {
     public CartItemAlreadyExistsException() {
         super("이미 장바구니에 담긴 상품입니다.");
     }
-
-    public CartItemAlreadyExistsException(String message) {
-        super(message);
-    }
 }
