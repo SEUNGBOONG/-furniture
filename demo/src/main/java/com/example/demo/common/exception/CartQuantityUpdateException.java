39876251367ca58d6085642369d0f66649ee3ef7
승엽 <tmdduqflfl@naver.com>
2025-06-25 18:48:48 +0900

    Refactoring: conflict

--- demo/src/main/java/com/example/demo/common/exception/CartQuantityUpdateException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.common.exception;
-
-public class CartQuantityUpdateException extends RuntimeException {
-    public CartQuantityUpdateException(String message) {
-        super(message);
-    }
-}
