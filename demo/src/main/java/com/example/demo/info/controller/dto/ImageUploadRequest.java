d651a35af27ce356beda8f73ea647a56e540bd80
승엽 <tmdduqflfl@naver.com>
2025-06-25 18:51:35 +0900

    Refactoring: 안쓰는 class 제거

--- demo/src/main/java/com/example/demo/info/controller/dto/ImageUploadRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.info.controller.dto;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public class ImageUploadRequest {
-    private MultipartFile file;  // 파일만 받음
-    // getter/setter
-}
--- demo/src/main/java/com/example/demo/info/controller/dto/NewsDTO.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.info.controller.dto;
-
-public record NewsDTO(
-        String url,
-        String title
-) {
-}
