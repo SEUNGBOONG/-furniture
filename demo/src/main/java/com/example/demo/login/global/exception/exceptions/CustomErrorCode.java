cb6e3a9d8459ff6d1c4767bc443eb0d7888ae63c
승엽 <tmdduqflfl@naver.com>
2025-06-29 00:10:40 +0900

    Refactoring: 캡슐화

--- demo/src/main/java/com/example/demo/login/global/exception/exceptions/CustomErrorCode.java
+++ demo/src/main/java/com/example/demo/login/global/exception/exceptions/CustomErrorCode.java
@@ -5,8 +5,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum CustomErrorCode {
 
-    NOT_FIND_TOKEN(HttpStatus.NOT_FOUND, "T001", "토큰을 찾을 수 없습니다."),
-    EXPIRED_TOKEN(HttpStatus.NOT_FOUND, "T002", "토큰 시간이 만료됐습니다."),
+    NOT_FIND_TOKEN(HttpStatus.UNAUTHORIZED, "T001", "토큰을 찾을 수 없습니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "T002", "토큰 시간이 만료됐습니다."),
     CART_ITEM_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "CART_001","이미 장바구니에 담긴 상품입니다."),
     CART_NOT_FOUND_ITEM(HttpStatus.BAD_REQUEST,"CART_OO2","장바구니 아이템이 없습니다.");
     private final HttpStatus httpStatus;
