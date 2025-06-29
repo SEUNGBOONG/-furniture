9d070f6650d9ee8614590a9fb49765420f8931c9
승엽 <tmdduqflfl@naver.com>
2025-06-29 15:58:11 +0900

    Refactoring: 캡슐화

--- demo/src/main/java/com/example/demo/info/controller/dto/CompanyHistoryImageDTO.java
+++ demo/src/main/java/com/example/demo/info/controller/dto/CompanyHistoryImageDTO.java
@@ -1,11 +1,8 @@
 package com.example.demo.info.controller.dto;
 
-import lombok.Getter;
-
 import java.io.Serial;
 import java.io.Serializable;
 
-@Getter
 public record CompanyHistoryImageDTO(String imageUrl1, String imageUrl2) implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
