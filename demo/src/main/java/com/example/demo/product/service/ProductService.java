5f2638d5d707a55a25fe7db47bece9737380d772
승엽 <tmdduqflfl@naver.com>
2025-07-06 23:52:04 +0900

    Refactoring: Redis ( ) -> ProductDetail () -> TTFB 70ms -> 20 ms

--- demo/src/main/java/com/example/demo/product/service/ProductService.java
+++ demo/src/main/java/com/example/demo/product/service/ProductService.java
@@ -12,6 +12,7 @@ import com.example.demo.product.domain.repository.CategoryRepository;
 import com.example.demo.product.domain.entity.Product;
 import com.example.demo.product.domain.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
