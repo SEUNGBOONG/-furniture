66a62e9918128fbf85082e313bfed6150a7ffedf
승엽 <tmdduqflfl@naver.com>
2025-07-08 17:13:25 +0900

    Feat : AdminService -> Controller

---
+++ demo/src/main/java/com/example/demo/login/member/controller/AdminMemberController.java
@@ -0,0 +1,25 @@
+package com.example.demo.login.member.controller;
+
+import com.example.demo.login.member.controller.auth.dto.MemberAdminResponse;
+import com.example.demo.login.member.service.auth.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/members")
+@RequiredArgsConstructor
+public class AdminMemberController {
+
+    private final AdminService adminService;
+
+    @GetMapping
+    public ResponseEntity<List<MemberAdminResponse>> getAllMembers() {
+        List<MemberAdminResponse> members = adminService.getAllMembers();
+        return ResponseEntity.ok(members);
+    }
+}
