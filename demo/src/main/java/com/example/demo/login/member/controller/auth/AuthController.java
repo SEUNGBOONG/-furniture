bcb751bf2b012848ef0997045795e3ded1dd7cb0
승엽 <tmdduqflfl@naver.com>
2025-07-11 16:33:08 +0900

    Refactoring: Authcontroller에 책임을 분산한다.

--- demo/src/main/java/com/example/demo/login/member/controller/auth/AuthController.java
+++ demo/src/main/java/com/example/demo/login/member/controller/auth/AuthController.java
@@ -1,30 +1,27 @@
 package com.example.demo.login.member.controller.auth;
 
 import com.example.demo.common.exception.Setting;
-import com.example.demo.login.email.util.EmailSenderUtil;
-import com.example.demo.login.member.controller.auth.dto.AuthRequestDTO;
-import com.example.demo.login.member.controller.auth.dto.ChangePasswordRequest;
-import com.example.demo.login.member.controller.auth.dto.EmailAuthRequestDto;
-import com.example.demo.login.member.controller.auth.dto.EmailCheckRequest;
 import com.example.demo.login.member.controller.auth.dto.LoginRequest;
 import com.example.demo.login.member.controller.auth.dto.LoginResponse;
-import com.example.demo.login.member.controller.auth.dto.NormalSignUpRequest;
 import com.example.demo.login.member.controller.auth.dto.SignUpRequest;
 import com.example.demo.login.member.controller.auth.dto.SignUpResponse;
+
+import com.example.demo.login.member.domain.auth.AuthUtil;
 import com.example.demo.login.member.mapper.auth.AuthMapper;
 import com.example.demo.login.member.service.auth.AuthService;
+
 import com.example.demo.login.util.CorporationValidator;
-import jakarta.mail.MessagingException;
+
 import jakarta.servlet.http.HttpSession;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URI;
 
 @RestController
@@ -34,7 +31,6 @@ import java.net.URI;
 public class AuthController {
 
     private final AuthService authService;
-    private final EmailSenderUtil emailSenderUtil;
     private final CorporationValidator corporationValidator;
 
     @PostMapping(value = "/members", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -43,9 +39,9 @@ public class AuthController {
             @RequestPart(value = "corporationImage") MultipartFile corporationImage,
             HttpSession session) {
 
-        Boolean isAuthenticated = (Boolean) session.getAttribute(getAuthKey(signUpRequest.memberEmail()));
-        ResponseEntity<String> PLEASE_COMPLETE_EMAIL_VERIFICATION_FIRST = getStringResponseEntity(isAuthenticated);
-        if (isaBoolean(PLEASE_COMPLETE_EMAIL_VERIFICATION_FIRST)) return PLEASE_COMPLETE_EMAIL_VERIFICATION_FIRST;
+        Boolean isAuthenticated = (Boolean) session.getAttribute(AuthUtil.getAuthKey(signUpRequest.memberEmail()));
+        ResponseEntity<String> PLEASE_COMPLETE_EMAIL_VERIFICATION_FIRST = AuthUtil.getStringResponseEntity(isAuthenticated);
+        if (AuthUtil.isaBoolean(PLEASE_COMPLETE_EMAIL_VERIFICATION_FIRST)) return PLEASE_COMPLETE_EMAIL_VERIFICATION_FIRST;
 
         Boolean isCorpAuthenticated = (Boolean) session.getAttribute(getCorpAuthKey(signUpRequest.corporationNumber()));
         if (signUpRequest.checkCorporation() && (isCorpAuthenticated == null || !isCorpAuthenticated)) {
@@ -53,55 +49,16 @@ public class AuthController {
         }
 
         SignUpResponse response = AuthMapper.toSignUpResponse(authService.signUp(signUpRequest, corporationImage));
-        session.removeAttribute(getAuthKey(signUpRequest.memberEmail()));
+        session.removeAttribute(AuthUtil.getAuthKey(signUpRequest.memberEmail()));
         session.removeAttribute(getCorpAuthKey(signUpRequest.corporationNumber()));
 
         URI location = URI.create("/members/" + response.id());
         return ResponseEntity.created(location).body(response);
     }
 
-    @PostMapping("/normalMembers")
-    public ResponseEntity<?> normalSignUp(@RequestBody NormalSignUpRequest request, HttpSession session) {
-
-        Boolean isAuthenticated = (Boolean) session.getAttribute(getAuthKey(request.memberEmail()));
-        ResponseEntity<String> PLEASE_COMPLETE_EMAIL_VERIFICATION_FIRST = getStringResponseEntity(isAuthenticated);
-        if (isaBoolean(PLEASE_COMPLETE_EMAIL_VERIFICATION_FIRST)) return PLEASE_COMPLETE_EMAIL_VERIFICATION_FIRST;
-
-        SignUpResponse response = AuthMapper.toSignUpResponse(authService.normalSignUp(request));
-        session.removeAttribute(getAuthKey(request.memberEmail()));
-
-        URI location = URI.create("/members/" + response.id());
-        return ResponseEntity.created(location).body(response);
-    }
-
-    private static boolean isaBoolean(final ResponseEntity<String> PLEASE_COMPLETE_EMAIL_VERIFICATION_FIRST) {
-        return PLEASE_COMPLETE_EMAIL_VERIFICATION_FIRST != null;
-    }
-
     @PostMapping(value = "/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
         return ResponseEntity.ok(authService.login(loginRequest));
-    }
-
-    @PostMapping("/send-auth-code")
-    public ResponseEntity<String> sendAuthCode(@RequestBody EmailAuthRequestDto emailDto, HttpSession session)
-            throws MessagingException, UnsupportedEncodingException {
-        String code = emailSenderUtil.sendEmail(emailDto.getEmail(), session);
-        session.setAttribute(emailDto.getEmail(), code);
-        session.setAttribute(getAuthKey(emailDto.getEmail()), false);
-        return ResponseEntity.ok(Setting.SUCCEED_SENDER_CERTIFICATION_NUMBER.toString());
-    }
-
-    @PostMapping("/verify-auth-code")
-    public ResponseEntity<String> verifyPasswordCode(@RequestBody AuthRequestDTO request, HttpSession session) {
-        String storedCode = (String) session.getAttribute(request.getEmail());
-        if (storedCode != null && storedCode.equals(request.getAuthCode())) {
-            session.removeAttribute(request.getEmail());
-            session.setAttribute(getAuthKey(request.getEmail()), true);
-
-            return ResponseEntity.ok(Setting.SUCCEED_CERTIFICATION_NUMBER.toString());
-        }
-        return ResponseEntity.badRequest().body(Setting.FAIL_CERTIFICATION_NUMBER.toString());
     }
 
     @PostMapping("/validate-corporation")
@@ -115,40 +72,8 @@ public class AuthController {
         }
     }
 
-    @PostMapping("/change-password")
-    public ResponseEntity<String> changePassword(@RequestBody ChangePasswordRequest request, HttpSession session) {
-        Boolean isAuthenticated = (Boolean) session.getAttribute(getAuthKey(request.getEmail()));
-        ResponseEntity<String> PLEASE_COMPLETE_EMAIL_VERIFICATION_FIRST = getStringResponseEntity(isAuthenticated);
-        if (isaBoolean(PLEASE_COMPLETE_EMAIL_VERIFICATION_FIRST)) return PLEASE_COMPLETE_EMAIL_VERIFICATION_FIRST;
-
-        authService.changePassword(request.getEmail(), request.getNewPassword(), request.getNewPasswordConfirm());
-        session.removeAttribute(getAuthKey(request.getEmail()));
-        return ResponseEntity.ok(Setting.PASSWORD_CHANGE_SUCCESS.toString());
-    }
-
-    @PostMapping("/check-email")
-    public ResponseEntity<String> checkEmail(@RequestBody EmailCheckRequest request) {
-        boolean isAvailable = authService.isEmailAvailable(request.getEmail());
-        if (isAvailable) {
-            return ResponseEntity.ok(Setting.EMAIL_AVAILABLE.toString());
-        } else {
-            return ResponseEntity.status(409).body(Setting.EMAIL_ALREADY_EXISTS.toString());
-        }
-    }
-
-    private String getAuthKey(String email) {
-        return Setting.AUTHENTICATED.toString() + ":" + email;
-    }
-
     private String getCorpAuthKey(String number) {
         return Setting.CORPORATION_AUTHENTICATED.toString() + ":" + number;
-    }
-
-    private static ResponseEntity<String> getStringResponseEntity(final Boolean isAuthenticated) {
-        if (isAuthenticated == null || !isAuthenticated) {
-            return ResponseEntity.status(403).body(Setting.PLEASE_COMPLETE_EMAIL_VERIFICATION_FIRST.toString());
-        }
-        return null;
     }
 
     @Data
