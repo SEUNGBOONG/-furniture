904bf2376a4133a5ccd60c333c2a16a3f7a62206
승엽 <tmdduqflfl@naver.com>
2025-07-28 15:14:18 +0900

    Feat: Member Error Code

--- demo/src/main/java/com/example/demo/mypage/payment/controller/dto/TossApproveRequest.java
+++ demo/src/main/java/com/example/demo/mypage/payment/controller/dto/TossApproveRequest.java
@@ -1,12 +1,12 @@
-package com.example.demo.mypage.payment.controller.dto;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class TossApproveRequest {
-    private String paymentKey;
-    private String orderId;
-    private int amount;
-}
+//package com.example.demo.mypage.payment.controller.dto;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//public class TossApproveRequest {
+//    private String paymentKey;
+//    private String orderId;
+//    private int amount;
+//}
--- demo/src/main/java/com/example/demo/mypage/payment/domain/repository/PaymentHistoryRepository.java
+++ demo/src/main/java/com/example/demo/mypage/payment/domain/repository/PaymentHistoryRepository.java
@@ -1,10 +1,10 @@
-package com.example.demo.mypage.payment.domain.repository;
-
-import com.example.demo.mypage.payment.domain.entity.PaymentHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface PaymentHistoryRepository extends JpaRepository<PaymentHistory, Long> {
-    Optional<PaymentHistory> findByOrderNumber(String orderNumber);
-}
+//package com.example.demo.mypage.payment.domain.repository;
+//
+//import com.example.demo.mypage.payment.domain.entity.PaymentHistory;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.Optional;
+//
+//public interface PaymentHistoryRepository extends JpaRepository<PaymentHistory, Long> {
+//    Optional<PaymentHistory> findByOrderNumber(String orderNumber);
+//}
