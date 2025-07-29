a34ac61fed96a6bf3deb611dc3ef868d4827a422
승엽 <tmdduqflfl@naver.com>
2025-07-29 17:05:14 +0900

    Feat: query -> N+1

---
+++ demo/src/main/java/com/example/demo/mypage/order/domain/repository/OrderRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.mypage.order.domain.repository;
+
+import com.example.demo.mypage.order.domain.entity.Order;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order, String> {
+
+    @Query("SELECT o FROM Order o JOIN FETCH o.products WHERE o.orderId = :orderId")
+    Optional<Order> findByOrderIdWithItems(@Param("orderId") String orderId);
+}
