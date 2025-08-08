80696dd318266712fe68de1a8976b6cd8fd7a9d7
승엽 <tmdduqflfl@naver.com>
2025-08-08 18:00:53 +0900

    Feat: 1. Logout Cookie 2. AuthMapper Token valid

--- demo/src/main/java/com/example/demo/login/member/controller/auth/dto/LoginResponse.java
+++ demo/src/main/java/com/example/demo/login/member/controller/auth/dto/LoginResponse.java
@@ -1,10 +1,8 @@
 package com.example.demo.login.member.controller.auth.dto;
 
 public record LoginResponse(
-        String token,
         Long memberId,
         String memberName,
         String memberNickName,
         boolean checkCorporation
-) {
-}
+) {}
