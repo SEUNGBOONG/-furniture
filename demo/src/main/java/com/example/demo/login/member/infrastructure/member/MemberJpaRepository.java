7ad60e9a4b9070cd40f27a3f4fcd0fb252f959f2
승엽 <tmdduqflfl@naver.com>
2025-09-08 16:28:12 +0900

    Feat : admin Member Normal Repository

--- demo/src/main/java/com/example/demo/login/member/infrastructure/member/MemberJpaRepository.java
+++ demo/src/main/java/com/example/demo/login/member/infrastructure/member/MemberJpaRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.login.member.infrastructure.member;
 
 import com.example.demo.login.member.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,4 +20,17 @@ public interface MemberJpaRepository extends JpaRepository<Member, Long> {
     Optional<Member> findById(Long memberId);
 
     List<Member> findAll();
+
+    @Query(value = "SELECT * FROM member m WHERE m.check_corporation = false LIMIT :limit OFFSET :offset", nativeQuery = true)
+    List<Member> findNormalMembers(int limit, int offset);
+
+    @Query(value = "SELECT COUNT(*) FROM member m WHERE m.check_corporation = false", nativeQuery = true)
+    long countNormalMembers();
+
+    // ✅ 사업자 회원 조회
+    @Query(value = "SELECT * FROM member m WHERE m.check_corporation = true LIMIT :limit OFFSET :offset", nativeQuery = true)
+    List<Member> findCorporateMembers(int limit, int offset);
+
+    @Query(value = "SELECT COUNT(*) FROM member m WHERE m.check_corporation = true", nativeQuery = true)
+    long countCorporateMembers();
 }
