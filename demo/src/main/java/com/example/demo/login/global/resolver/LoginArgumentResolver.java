320fd02634a761d4cf0b55a638b879f41fed15e5
승엽 <tmdduqflfl@naver.com>
2025-09-18 17:04:40 +0900

    Fix: token

--- demo/src/main/java/com/example/demo/login/global/resolver/LoginArgumentResolver.java
+++ demo/src/main/java/com/example/demo/login/global/resolver/LoginArgumentResolver.java
@@ -43,8 +43,7 @@ public class LoginArgumentResolver implements HandlerMethodArgumentResolver {
 
         if (token == null && request.getCookies() != null) {
             token = Arrays.stream(request.getCookies())
-                    .filter(cookie ->
-                            "token".equals(cookie.getName()) || "accessToken".equals(cookie.getName()))
+                    .filter(cookie -> "token".equals(cookie.getName()))
                     .findFirst()
                     .map(Cookie::getValue)
                     .orElse(null);
