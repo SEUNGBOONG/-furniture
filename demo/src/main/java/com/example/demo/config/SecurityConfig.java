28a7648ba7e91bda6b468370b8cf55f7fd8e9b04
승엽 <tmdduqflfl@naver.com>
2025-09-22 16:20:41 +0900

    Refactoring : admin -> login

--- demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -4,22 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import com.example.demo.login.member.infrastructure.auth.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final CorsConfig corsConfig; // ✅ 주입받음
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .logout(logout -> logout.disable())
+                .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource())) // ✅ CORS 적용
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 )
